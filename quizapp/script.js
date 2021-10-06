d747c9e01458d223bafbe9091ddd772d9b0b21a7
Alexander Jobmann <jobmann.dev@gmail.com>
2021-10-06 19:46:22 +0200

    finish

--- quizapp/script.js
+++ quizapp/script.js
@@ -67,21 +67,33 @@ let questions = [
 ]
 let currentQuestion = 0;
 let rightQuestions = 0;
-
+let audio_sucess=new Audio('audio/success.mp3');
+let audio_fail=new Audio('audio/fail.mp3');
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
     showQuestion();
 }
 function showQuestion() {
-    if (currentQuestion >= questions.length) { //show endscreen
+    if (gameIsOver()) { //show endscreen
         showEndscreen();
 
     } else {//show question
-        let percent = (currentQuestion+1)/questions.length;
-        percent=percent *100;
-        document.getElementById('progress-bar').style.width=`${percent}%`;
-        document.getElementById('progress-bar').innerHTML=`${percent}%`;
+        updateProgressBar();
+        updateToNextQuestion();
+    }
+}
+function gameIsOver(){
+    return currentQuestion >= questions.length;
+}
+function updateProgressBar(){
+    let percent = (currentQuestion+1)/questions.length;
+    percent=percent *100;
+    document.getElementById('progress-bar').style.width=`${percent}%`;
+    document.getElementById('progress-bar').innerHTML=`${percent}%`;
+}
+
+function updateToNextQuestion(){
         let question = questions[currentQuestion];
         document.getElementById('question-number').innerHTML = currentQuestion + 1;
         document.getElementById('question-text').innerHTML = question['question'];
@@ -89,19 +101,19 @@ function showQuestion() {
         document.getElementById('answer_2').innerHTML = question['answer_2'];
         document.getElementById('answer_3').innerHTML = question['answer_3'];
         document.getElementById('answer_4').innerHTML = question['answer_4'];
-    }
 }
-
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
     if (selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        audio_sucess.play();
         rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        audio_fail.play();
     }
     document.getElementById('next-question').disabled = false;
 }
